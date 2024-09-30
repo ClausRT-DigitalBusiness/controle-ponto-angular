dedad07f1b0d205a9f7b6882b60a822873d5f82e
Claus Tessmann <claus@l2code.com.br>
2024-09-30 17:31:45 -0300

    Limpo app.component.html e atualizado app.routes.ts.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { routes as AuthRoutes } from 'modules/auth/auth.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  ...AuthRoutes,
+];
