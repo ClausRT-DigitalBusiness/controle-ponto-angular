744566fcf2624af6dfa7162e00536ca3d7401f62
Claus Tessmann <claus@l2code.com.br>
2024-10-01 10:27:51 -0300

    WiP: tratamento de erro para passar a mensagem para o usuário.

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { firstValueFrom } from 'rxjs';
 import { AuthLayoutComponent } from '../shared/components/auth-layout/auth-layout.component';
 import { AuthService } from '../shared/services/auth.service';
-import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -18,8 +19,10 @@ import { firstValueFrom } from 'rxjs';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private _snackBar = inject(MatSnackBar);
+
   public loginForm!: FormGroup;
-  public loading = true;
+  public loading = false;
   public showPassword = false;
 
   constructor(private authService: AuthService, private fb: FormBuilder) {
@@ -29,20 +32,31 @@ export class LoginComponent {
     });
   }
 
-  async onSubmit() {
+  // TODO melhorar tratamento de erro de API
+  private errorHandler(error: any) {
+    let message = 'An error occurred. Please try again.';
+
+    if (error.message) {
+      message = error.message;
+    }
+
+    this._snackBar.open(message, 'Close');
+  }
+
+  public async onSubmit() {
     try {
       this.loading = true;
       const observer = this.authService.login(this.loginForm.value);
       const { token } = await firstValueFrom(observer);
       console.log(token); // TODO salvar isso num estado superior ou localStore para ser usado em outros services
     } catch (error) {
-      console.log(error); // TODO jogar mensagem de erro para tela
+      this.errorHandler(error);
     } finally {
       this.loading = false;
     }
   }
 
-  togglePasswordVisibility() {
+  public togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
 }
