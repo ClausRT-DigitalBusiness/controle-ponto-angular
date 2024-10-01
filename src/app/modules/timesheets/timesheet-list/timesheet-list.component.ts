1f3f9b7ecaacc5bc1cbfe7b273ee54fb41ba84b6
Claus Tessmann <claus@l2code.com.br>
2024-10-01 10:28:04 -0300

    WiP: placeholder para módulo de timesheet.

---
+++ src/app/modules/timesheets/timesheet-list/timesheet-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timesheet-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './timesheet-list.component.html',
+  styleUrl: './timesheet-list.component.scss'
+})
+export class TimesheetListComponent {
+
+}
